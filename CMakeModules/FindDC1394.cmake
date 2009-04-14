5fed57431442976c44a3251cfdcef31192b2e0b5
Andrew Straw <strawman@astraw.com>
2009-04-13 20:30:16 -0700

    Use cmake to compile/install libcamiface (initial preliminary support)

---
+++ CMakeModules/FindDC1394.cmake
@@ -0,0 +1,9 @@
+FIND_PATH(DC1394_INCLUDE_PATH control.h
+  /usr/local/include/dc1394
+  /usr/include/dc1394
+)
+
+FIND_LIBRARY(DC1394_LIBRARY dc1394 /usr/local/lib /usr/lib)
+
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(DC1394 DEFAULT_MSG DC1394_INCLUDE_PATH DC1394_LIBRARY)
