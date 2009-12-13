40af3895c059e50d3af043061294075e0b40ece2
Andrew Straw <strawman@astraw.com>
2009-12-13 11:40:28 -0800

    build flycapture2 backend on 32-bit and 64-bit windows

--- CMakeModules/FindFlyCapture.cmake
+++ CMakeModules/FindFlyCapture.cmake
@@ -1,7 +1,17 @@
 # Get default values for FLYCAPTURE_ROOT and FLYCAPTURE_LAYOUT
 
 IF(WIN32)
-  SET(FLYCAPTURE_ROOT "C:\\Program Files\\Point Grey Research\\FlyCapture2")
+  IF(EXISTS "C:\\Program Files\\Point Grey Research\\FlyCapture2")
+    SET(FLYCAPTURE_ROOT "C:\\Program Files\\Point Grey Research\\FlyCapture2")
+  ELSE(EXISTS "C:\\Program Files\\Point Grey Research\\FlyCapture2")
+    IF(EXISTS "C:\\Program Files (x86)\\Point Grey Research\\FlyCapture2")
+      # Maybe we're on 64bit but have 32bit SDK installed.
+      SET(FLYCAPTURE_ROOT "C:\\Program Files (x86)\\Point Grey Research\\FlyCapture2")
+    ELSE(EXISTS "C:\\Program Files (x86)\\Point Grey Research\\FlyCapture2")
+      # If not, default to "C:\Program Files".
+      SET(FLYCAPTURE_ROOT "C:\\Program Files\\Point Grey Research\\FlyCapture2")
+    ENDIF(EXISTS "C:\\Program Files (x86)\\Point Grey Research\\FlyCapture2")
+  ENDIF(EXISTS "C:\\Program Files\\Point Grey Research\\FlyCapture2")
 ELSE(WIN32)
   # Assume headers and libraries are installed in FHS locations
   SET(FLYCAPTURE_ROOT "/usr")
@@ -28,7 +38,13 @@ SET(FLYCAPTURE_TEST_LIB_PATHS "$ENV{FLYCAPTURE_TEST_LIB_PATHS}")
 IF(FLYCAPTURE_TEST_LIB_PATHS STREQUAL "")
   # not set with env var, use defaults
   IF(FLYCAPTURE_LAYOUT STREQUAL "flycapture")
-    SET(FLYCAPTURE_TEST_LIB_PATHS ${FLYCAPTURE_ROOT}/lib64)
+    IF(EXISTS ${FLYCAPTURE_ROOT}/lib64)
+      SET(FLYCAPTURE_TEST_LIB_PATHS ${FLYCAPTURE_ROOT}/lib64)
+      MESSAGE("building FlyCapture2 64-bit backend")
+    ELSE(EXISTS ${FLYCAPTURE_ROOT}/lib64)
+      SET(FLYCAPTURE_TEST_LIB_PATHS ${FLYCAPTURE_ROOT}/lib)
+      MESSAGE("building FlyCapture2 32-bit backend")
+    ENDIF(EXISTS ${FLYCAPTURE_ROOT}/lib64)
   ELSE(FLYCAPTURE_LAYOUT STREQUAL "flycapture")
     IF(NOT FLYCAPTURE_LAYOUT STREQUAL "FHS")
       MESSAGE(FATAL_ERROR "unknown FLYCAPTURE_LAYOUT " ${FLYCAPTURE_LAYOUT})
