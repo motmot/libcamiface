63063c86d202485995b646d76a7c70e1deb0efe8
Andrew Straw <strawman@astraw.com>
2009-12-13 20:23:50 -0800

    remove compiler warning from function declarations

--- src/cam_iface_static_functions.h
+++ src/cam_iface_static_functions.h
@@ -1,13 +1,13 @@
 /* internal structures for mega backend implementation */
 
-const char *BACKEND_METHOD(cam_iface_get_driver_name)();
+const char *BACKEND_METHOD(cam_iface_get_driver_name)(void);
 
-void BACKEND_METHOD(cam_iface_clear_error)();
-int BACKEND_METHOD(cam_iface_have_error)();
-const char * BACKEND_METHOD(cam_iface_get_error_string)();
-void BACKEND_METHOD(cam_iface_startup)();
-void BACKEND_METHOD(cam_iface_shutdown)();
-int BACKEND_METHOD(cam_iface_get_num_cameras)();
+void BACKEND_METHOD(cam_iface_clear_error)(void);
+int BACKEND_METHOD(cam_iface_have_error)(void);
+const char * BACKEND_METHOD(cam_iface_get_error_string)(void);
+void BACKEND_METHOD(cam_iface_startup)(void);
+void BACKEND_METHOD(cam_iface_shutdown)(void);
+int BACKEND_METHOD(cam_iface_get_num_cameras)(void);
 void BACKEND_METHOD(cam_iface_get_camera_info)(int device_number, Camwire_id *out_camid);
 void BACKEND_METHOD(cam_iface_get_num_modes)(int device_number, int *num_modes);
 void BACKEND_METHOD(cam_iface_get_mode_string)(int device_number,
